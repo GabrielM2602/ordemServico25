db1f0116a69cc8659fcc22023d8a733a0c27c706
maxmiliano <maxmilianotavareszaina@gmail.com>
2025-08-08 12:48:11 -0300

    backup

---
+++ Dump20250808.sql
@@ -0,0 +1,119 @@
+CREATE DATABASE  IF NOT EXISTS `dbos2025` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `dbos2025`;
+-- MySQL dump 10.13  Distrib 8.0.34, for Win64 (x86_64)
+--
+-- Host: localhost    Database: dbos2025
+-- ------------------------------------------------------
+-- Server version	8.0.34
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `tbclientes`
+--
+
+DROP TABLE IF EXISTS `tbclientes`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `tbclientes` (
+  `id_cliente` int NOT NULL AUTO_INCREMENT,
+  `nomecli` varchar(50) NOT NULL,
+  `endecli` varchar(100) DEFAULT NULL,
+  `fonecli` varchar(15) NOT NULL,
+  `emailcli` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`id_cliente`),
+  UNIQUE KEY `emailcli` (`emailcli`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tbclientes`
+--
+
+LOCK TABLES `tbclientes` WRITE;
+/*!40000 ALTER TABLE `tbclientes` DISABLE KEYS */;
+INSERT INTO `tbclientes` VALUES (1,'slk não compensa','Rua 18','66123321222','slk@gmail.com');
+/*!40000 ALTER TABLE `tbclientes` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `tbos`
+--
+
+DROP TABLE IF EXISTS `tbos`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `tbos` (
+  `os` int NOT NULL AUTO_INCREMENT,
+  `data_os` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `tipo` varchar(15) NOT NULL,
+  `situacao` varchar(20) NOT NULL,
+  `equipamento` varchar(150) NOT NULL,
+  `defeito` varchar(150) DEFAULT NULL,
+  `servico` varchar(150) DEFAULT NULL,
+  `tecnico` varchar(30) DEFAULT NULL,
+  `valor` decimal(10,2) DEFAULT NULL,
+  `id_cliente` int NOT NULL,
+  PRIMARY KEY (`os`),
+  KEY `id_cliente` (`id_cliente`),
+  CONSTRAINT `tbos_ibfk_1` FOREIGN KEY (`id_cliente`) REFERENCES `tbclientes` (`id_cliente`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tbos`
+--
+
+LOCK TABLES `tbos` WRITE;
+/*!40000 ALTER TABLE `tbos` DISABLE KEYS */;
+/*!40000 ALTER TABLE `tbos` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `tbusuarios`
+--
+
+DROP TABLE IF EXISTS `tbusuarios`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `tbusuarios` (
+  `id_user` int NOT NULL,
+  `usuario` varchar(15) NOT NULL,
+  `fone` varchar(15) DEFAULT NULL,
+  `login` varchar(15) NOT NULL,
+  `senha` varchar(250) NOT NULL,
+  `perfil` varchar(20) NOT NULL,
+  PRIMARY KEY (`id_user`),
+  UNIQUE KEY `login` (`login`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tbusuarios`
+--
+
+LOCK TABLES `tbusuarios` WRITE;
+/*!40000 ALTER TABLE `tbusuarios` DISABLE KEYS */;
+INSERT INTO `tbusuarios` VALUES (1,'Gabriel','6640028922','Gabriel','123321','Admin');
+/*!40000 ALTER TABLE `tbusuarios` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2025-08-08 12:46:19
